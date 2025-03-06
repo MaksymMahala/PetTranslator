3361a53b8af39ec0b5face2123fd58bec9456e05
maksym.mahala <maksym.mahala.kn.2021@lpnu.ua>
2025-03-06 18:49:36 +0200

    Changed color

--- TestApp/UI/Translator/TranslatorView.swift
+++ TestApp/UI/Translator/TranslatorView.swift
@@ -32,7 +32,7 @@ struct TranslatorView: View {
     }
     
     private var background: some View {
-        LinearGradient(gradient: Gradient(colors: [Color.lightGreenColor.opacity(0.2), Color.lightGreenColor]), startPoint: .top, endPoint: .bottom)
+        LinearGradient(gradient: Gradient(colors: [Color.white, Color.lightGreenColor]), startPoint: .top, endPoint: .bottom)
             .ignoresSafeArea()
     }
     
